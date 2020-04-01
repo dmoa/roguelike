f70b0ae4de98f0742fb9011311ce56a0a9f6a520
Stan <spammyboy@protonmail.com>
2020-04-01 16:49:53 +0100

    tilemap renderer fixed and working with map data

--- src/Map.hpp
+++ src/Map.hpp
@@ -1,12 +1,15 @@
 #pragma once
 
+#include <TileMapRenderer/TileMapRenderer.hpp>
+
 class Map {
 public:
 	Map();
 	void Update();
 	void Draw(sf::RenderTexture* renderTexture);
-	sf::Sprite getSprite();
 private:
 	sf::Texture m_texture;
 	sf::Sprite m_sprite;
+	int tileLength;
+	TileMapRenderer mapRenderer;
 };
--- src/States/src/Game.cpp
+++ src/States/src/Game.cpp
@@ -1,10 +1,14 @@
 #include "../include/Game.hpp"
 
 extern const int SCALE;
+extern const int GAME_WIDTH;
+extern const int GAME_HEIGHT;
 
 Game::Game(sf::Shader* _shader)
 {
 	m_shader = _shader;
+	m_renderTexture.create(GAME_WIDTH, GAME_HEIGHT);
+	m_sprite.setScale(SCALE, SCALE);
 }
 
 // Update function contains state-specific logic
@@ -16,6 +20,7 @@ void Game::Update(sf::Time deltaTime)
 // Draw function contains SFML draw calls
 void Game::Draw(const std::shared_ptr<sf::RenderWindow>& window)
 {
+	m_renderTexture.clear();
 	m_map.Draw(&m_renderTexture);
 	m_renderTexture.display();
 	m_sprite.setTexture(m_renderTexture.getTexture());
