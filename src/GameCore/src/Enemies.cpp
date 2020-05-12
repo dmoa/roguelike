162d7d159ebb0ea1c6362a743ce1b7b4e3af2b98
Stan <spammyboy@protonmail.com>
2020-05-12 19:58:53 +0100

    more enemies

--- src/GameCore/src/Enemies.cpp
+++ src/GameCore/src/Enemies.cpp
@@ -22,35 +22,49 @@ Enemies::Enemies()
 
     m_enemyTypes[5].viewType = Left;
     m_enemyTypes[5].shapes.push_back(sf::ConvexShape(3));
-    m_enemyTypes[5].shapes[0].setPoint(0, sf::Vector2f(1, 21));
-    m_enemyTypes[5].shapes[0].setPoint(1, sf::Vector2f(41, 1));
-    m_enemyTypes[5].shapes[0].setPoint(2, sf::Vector2f(41, 41));
+    m_enemyTypes[5].shapes[0].setPoint(0, sf::Vector2f(0, 40));
+    m_enemyTypes[5].shapes[0].setPoint(1, sf::Vector2f(80, 0));
+    m_enemyTypes[5].shapes[0].setPoint(2, sf::Vector2f(80, 80));
     m_enemyTypes[5].shapes[0].setFillColor(sf::Color(191,97,106));
-    m_enemyTypes[5].width = 20;
-    m_enemyTypes[5].height = 20;
+    m_enemyTypes[5].width = 80;
+    m_enemyTypes[5].height = 80;
 
     m_enemyTypes[6].viewType = Right;
     m_enemyTypes[6].shapes.push_back(sf::ConvexShape(3));
-    m_enemyTypes[6].shapes[0].setPoint(0, sf::Vector2f(21, 0));
-    m_enemyTypes[6].shapes[0].setPoint(1, sf::Vector2f(41, 41));
-    m_enemyTypes[6].shapes[0].setPoint(2, sf::Vector2f(1, 41));
+    m_enemyTypes[6].shapes[0].setPoint(0, sf::Vector2f(0, 0));
+    m_enemyTypes[6].shapes[0].setPoint(1, sf::Vector2f(0, 80));
+    m_enemyTypes[6].shapes[0].setPoint(2, sf::Vector2f(80, 40));
     m_enemyTypes[6].shapes[0].setFillColor(sf::Color(191,97,106));
-    m_enemyTypes[6].width = 20;
-    m_enemyTypes[6].height = 20;
+    m_enemyTypes[6].width = 80;
+    m_enemyTypes[6].height = 80;
 
     m_enemyTypes[7].viewType = Vertical;
     m_enemyTypes[7].shapes.push_back(sf::ConvexShape(3));
-    m_enemyTypes[7].shapes[0].setPoint(0, sf::Vector2f(21, 0));
-    m_enemyTypes[7].shapes[0].setPoint(1, sf::Vector2f(41, 41));
-    m_enemyTypes[7].shapes[0].setPoint(2, sf::Vector2f(1, 41));
+    m_enemyTypes[7].shapes[0].setPoint(0, sf::Vector2f(40, 0));
+    m_enemyTypes[7].shapes[0].setPoint(1, sf::Vector2f(80, 30));
+    m_enemyTypes[7].shapes[0].setPoint(2, sf::Vector2f(0, 30));
     m_enemyTypes[7].shapes[0].setFillColor(sf::Color(191,97,106));
+    m_enemyTypes[7].shapes.push_back(sf::ConvexShape(3));
+    m_enemyTypes[7].shapes[1].setPoint(0, sf::Vector2f(0, 50));
+    m_enemyTypes[7].shapes[1].setPoint(1, sf::Vector2f(80, 50));
+    m_enemyTypes[7].shapes[1].setPoint(2, sf::Vector2f(40, 80));
+    m_enemyTypes[7].shapes[1].setFillColor(sf::Color(191,97,106));
+    m_enemyTypes[7].width = 80;
+    m_enemyTypes[7].height = 80;
 
     m_enemyTypes[8].viewType = Horizontal;
     m_enemyTypes[8].shapes.push_back(sf::ConvexShape(3));
-    m_enemyTypes[8].shapes[0].setPoint(0, sf::Vector2f(21, 0));
-    m_enemyTypes[8].shapes[0].setPoint(1, sf::Vector2f(41, 41));
-    m_enemyTypes[8].shapes[0].setPoint(2, sf::Vector2f(1, 41));
+    m_enemyTypes[8].shapes[0].setPoint(0, sf::Vector2f(0, 40));
+    m_enemyTypes[8].shapes[0].setPoint(1, sf::Vector2f(30, 0));
+    m_enemyTypes[8].shapes[0].setPoint(2, sf::Vector2f(30, 80));
     m_enemyTypes[8].shapes[0].setFillColor(sf::Color(191,97,106));
+    m_enemyTypes[8].shapes.push_back(sf::ConvexShape(3));
+    m_enemyTypes[8].shapes[1].setPoint(0, sf::Vector2f(50, 0));
+    m_enemyTypes[8].shapes[1].setPoint(1, sf::Vector2f(80, 40));
+    m_enemyTypes[8].shapes[1].setPoint(2, sf::Vector2f(50, 80));
+    m_enemyTypes[8].shapes[1].setFillColor(sf::Color(191,97,106));
+    m_enemyTypes[8].width = 80;
+    m_enemyTypes[8].height = 80;
 
     m_enemyTypes[9].viewType = Rook;
     m_enemyTypes[9].shapes.push_back(sf::ConvexShape(3));
@@ -58,6 +72,8 @@ Enemies::Enemies()
     m_enemyTypes[9].shapes[0].setPoint(1, sf::Vector2f(41, 41));
     m_enemyTypes[9].shapes[0].setPoint(2, sf::Vector2f(1, 41));
     m_enemyTypes[9].shapes[0].setFillColor(sf::Color(191,97,106));
+    m_enemyTypes[9].width = 80;
+    m_enemyTypes[9].height = 80;
 }
 
 void Enemies::Setup(LevelManager* levelManager, sf::Vector2f playerPos)
