1d18983a0f0f7a5408a1045fa42ccdd3e7a9555e
Stan O <spammyboy@protonmail.com>
2020-05-31 00:19:09 +0100

    bad idea reversed

--- lib/Collision/Collision.hpp
+++ lib/Collision/Collision.hpp
@@ -3,8 +3,8 @@
 class Collision
 {
 public:
-	static bool PointInRect(sf::Vector2f point, sf::RectangleShape rect)
+	static bool PointInRect(sf::Vector2f point, sf::FloatRect rect)
 	{
-		return point.x > rect.getPosition().x && point.x < rect.getPosition().x + rect.getLocalBounds().width && point.y > rect.getPosition().y && point.y < rect.getPosition().y + rect.getLocalBounds().height;
+		return point.x > rect.left && point.x < rect.left + rect.width && point.y > rect.top && point.y < rect.top + rect.height;
 	}
 };
