e5c26ddb4fe4fecbf474be40875b0f00066ee415
Stan O <spammyboy@protonmail.com>
2020-05-31 00:19:30 +0100

    collision detection + selectable enemies to use

--- src/LevelMaker/LevelMaker.cpp
+++ src/LevelMaker/LevelMaker.cpp
@@ -6,6 +6,7 @@ LevelMaker::LevelMaker(sf::RenderWindow* renderWindow, LevelManager* levelManage
 	m_levelManager = levelManager;
 	m_player = player;
 	m_enemies = enemies;
+	m_enemyTypes = enemies->GetEnemyTypes();
 
 	m_levelRender.texture.create(levelManager->GetLevelWidth(), levelManager->GetLevelHeight());
 	m_levelRender.bg = sf::Color(34, 35, 35);
@@ -13,16 +14,23 @@ LevelMaker::LevelMaker(sf::RenderWindow* renderWindow, LevelManager* levelManage
 	m_levelRender.sprite.setScale(m_levelRender.scale, m_levelRender.scale);
 
 	m_cursorMode = Drawing;
-	m_selectedItemIndex = 1;
+	m_selectedItemIndex = 0;
 
-	temp_shape.setSize(sf::Vector2f(100, 50));
-	temp_shape.setPosition(15, 180);
-	temp_shape.setFillColor(sf::Color::Yellow);
+	m_commonBorder = 30;
+	m_enemySelectorWidth = (*m_enemyTypes)[3].width + m_commonBorder;
+
+	m_modeSelectorShape.setSize(sf::Vector2f(100, 50));
+	m_modeSelectorShape.setPosition(m_commonBorder, 180);
+	m_modeSelectorShape.setFillColor(sf::Color::Yellow);
+
+	m_selectedShapeOutline.setSize(sf::Vector2f(m_commonBorder / 2, m_commonBorder / 2));
+	m_selectedShapeOutline.setFillColor(sf::Color::White);
+	m_selectedShapeOutline.setPosition((m_selectedItemIndex + 0.5) * m_enemySelectorWidth + m_commonBorder - m_selectedShapeOutline.getSize().x / 2, m_commonBorder / 2 - m_selectedShapeOutline.getSize().y / 2);
 
 	m_font.loadFromFile("content/fonts/stats.ttf");
 	m_details.setFont(m_font);
 	m_details.setCharacterSize(18);
-	m_details.setPosition(15, 130);
+	m_details.setPosition(m_commonBorder, 130);
 	m_details.setString("mode: draw\nselected: " + std::to_string(m_selectedItemIndex));
 	m_details.setFillColor(sf::Color::White);
 }
@@ -36,15 +44,15 @@ void LevelMaker::Draw()
 	m_player->Draw(&m_levelRender.texture);
 
 	// drawing enemy options to choose from
-	for (unsigned int i = 0; i < m_enemies->GetEnemyTypes()->size(); i++)
+	for (unsigned int i = 0; i < m_enemyTypes->size(); i++)
 	{
-		EnemyProperties tempEnemy = (*(m_enemies->GetEnemyTypes()))[i];
+		EnemyProperties tempEnemy = (*m_enemyTypes)[i];
 		for (unsigned int j = 0; j < tempEnemy.shapes.size(); j ++)
 		{
 			// meh fix
 			// i - 3 because the map actually starts indexing at 9
 			// might be good to add k variable if we get lots of enemies
-			tempEnemy.shapes[j].setPosition((i - 3) * (tempEnemy.width + 15) + 15, 15);
+			tempEnemy.shapes[j].setPosition((i - 3) * m_enemySelectorWidth + m_commonBorder * 1.5, m_commonBorder);
 			m_window->draw(tempEnemy.shapes[j]);
 		}
 
@@ -56,8 +64,9 @@ void LevelMaker::Draw()
 	m_levelRender.sprite.setPosition((m_window->getSize().x - m_levelManager->GetLevelWidth() * m_levelRender.scale) / 2, (m_window->getSize().y - m_levelManager->GetLevelHeight() * m_levelRender.scale) / 2);
 
 	m_window->draw(m_levelRender.sprite);
-	m_window->draw(temp_shape);
+	m_window->draw(m_modeSelectorShape);
 	m_window->draw(m_details);
+	m_window->draw(m_selectedShapeOutline);
 }
 
 void LevelMaker::Update(std::vector<sf::Event>* events)
@@ -66,10 +75,27 @@ void LevelMaker::Update(std::vector<sf::Event>* events)
 	{
 		if ((*events)[i].type == sf::Event::MouseButtonPressed)
 		{
-			if (Collision::PointInRect(sf::Vector2f(sf::Mouse::getPosition(*m_window)), temp_shape))
+			sf::Vector2f mouse_pos = sf::Vector2f(sf::Mouse::getPosition(*m_window));
+			if (Collision::PointInRect(mouse_pos, m_modeSelectorShape.getGlobalBounds()))
 			{
 				m_cursorMode = (m_cursorMode == Drawing) ? Erase : Drawing;
 				UpdateToolsRender();
+			}
+			// all enemies have a height of 80, this is temp fix, because enemy heights could change.
+			// ideally, we would figure out the max enemy height earlier and use that.
+			if (mouse_pos.y > m_commonBorder && mouse_pos.y < m_commonBorder + 80)
+			{
+				int j;
+				for (unsigned int i = 0; i < m_enemyTypes->size(); i++)
+				{
+					j = i - 3; // for everything but indexing (becaue map starts at 2, will have to fix this at some point)
+					EnemyProperties ep = (*m_enemyTypes)[i];
+					if (Collision::PointInRect(mouse_pos, sf::FloatRect(m_commonBorder + m_enemySelectorWidth * j, m_commonBorder, m_enemySelectorWidth, (*m_enemyTypes)[i].height)))
+					{
+						m_selectedItemIndex = j;
+						UpdateToolsRender();
+					}
+				}
 			}
 		}
 	}
@@ -77,7 +103,8 @@ void LevelMaker::Update(std::vector<sf::Event>* events)
 
 void LevelMaker::UpdateToolsRender()
 {
-	temp_shape.setFillColor(m_cursorMode == Drawing ? sf::Color::Yellow : sf::Color::White);
+	m_modeSelectorShape.setFillColor(m_cursorMode == Drawing ? sf::Color::Yellow : sf::Color::White);
+	m_selectedShapeOutline.setPosition((m_selectedItemIndex + 0.5) * m_enemySelectorWidth + m_commonBorder - m_selectedShapeOutline.getSize().x / 2, m_commonBorder / 2 - m_selectedShapeOutline.getSize().y / 2);
 
 	std::string new_string = "mode: ";
 	new_string += (m_cursorMode == Drawing) ? "draw" : "erase";
--- src/LevelMaker/LevelMaker.hpp
+++ src/LevelMaker/LevelMaker.hpp
@@ -23,14 +23,18 @@ public:
 private:
 	void UpdateToolsRender();
 	std::vector<std::string> Split(std::string str, char delim = '\n');
-	sf::RenderWindow* m_window;
 	Render m_levelRender;
-	CursorModes m_cursorMode;
-	int m_selectedItemIndex;
+	sf::RenderWindow* m_window;
 	LevelManager* m_levelManager;
 	Player* m_player;
 	Enemies* m_enemies;
-	sf::RectangleShape temp_shape;
+	std::map<int, EnemyProperties>* m_enemyTypes;
+	CursorModes m_cursorMode;
+	int m_selectedItemIndex;
+	int m_commonBorder;
+	int m_enemySelectorWidth;
 	sf::Font m_font;
 	sf::Text m_details;
+	sf::RectangleShape m_modeSelectorShape;
+	sf::RectangleShape m_selectedShapeOutline;
 };
